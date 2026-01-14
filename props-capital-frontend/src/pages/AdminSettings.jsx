91ff220e4eb31e3b6ba3c7f40ad08f1238316cde
Salahuddin33102 <nabeeltahirdeveloper@gmail.com>
2026-01-14 19:50:43 +0500

    deploy

--- props-capital-frontend/src/pages/AdminSettings.jsx
+++ props-capital-frontend/src/pages/AdminSettings.jsx
@@ -183,7 +183,7 @@ export default function AdminSettings() {
             {saveMutation.isPending ? (
               <>
                 <Loader2 className="w-4 h-4 mr-2 animate-spin" />
-                {t("admin.settings.saving", { defaultValue: "Saving..." })}
+                {t("admin.settings.saveChange", { defaultValue: "Saving..." })}
               </>
             ) : (
               <>
